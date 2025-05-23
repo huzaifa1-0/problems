9011dea1c3c2a58cdf42a9327a33a0caad358c7c
Huzaifa Rashid <huzaifaras10@gmail.com>
2025-05-23 05:20:20 -0700

    second

--- data simulator.py
+++ data simulator.py
@@ -12,4 +12,22 @@ class dataset:
         total = sum(self.data)
         return total/len(self.data)
 
+    def median(self):
+        if not self.data:
+            return None
+        sorted_data = sorted(self.data)
+        n = len(sorted_data)
+        mid = n//2
+        if n%2 == 0:
+            return (sorted_data[mid-1]+ sorted_data[mid])/2
+        else:
+            return sorted_data[mid]
+    def standard_deviation(self):
+        if not self.data:
+            return None
+        mean_value = self.mean()
+        variance = sum([(x-mean_value)**2 for x in self.data])/len(self.data)
+        return variance**0.5
 
+    def add_noise(self,magnitude):
+        self.data =
