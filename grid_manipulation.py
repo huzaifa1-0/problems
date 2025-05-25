6329659fabaee2c6747776bcaac1028f56fc667f
Huzaifa Rashid <huzaifaras10@gmail.com>
2025-05-24 17:27:21 -0700

    second

--- grid_manipulation.py
+++ grid_manipulation.py
@@ -12,3 +12,11 @@ class imagematrix:
             for j in range(n):
                 rotated[j][n-1-i] = self.matrix[i][j]
         self.matrix = rotated
+
+    def horizontalflip(self):
+        n = self.n
+        flip = [[0]^n for _ in range(n)]
+        for i in range(n):
+            for j in range(n):
+                flip[i][j] = self.matrix[i][n-1-j]
+        self.matrix = flip
