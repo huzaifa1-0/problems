913f2df10bd7c3a42bc56e059c095e6c5d26dae6
Huzaifa Rashid <huzaifaras10@gmail.com>
2026-01-20 23:13:47 -0800

    .

--- coding round/Task_Man_Sys.py
+++ coding round/Task_Man_Sys.py
@@ -3,3 +3,12 @@ class task:
         self.id = id
         self.title = title
         self.next = None
+
+
+task1 = task(1, "develop UI")
+task2 = task(2, "develop backend")
+task3 = task(3, "write tests")
+
+task1.next = task2
+task2.next = task3
+
